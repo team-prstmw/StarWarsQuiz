4894a879af517579ffd782d20fa5db4d14cce9dd
Marta Goniszewska <marta.goniszewska@outlook.com>
2021-12-01 11:59:05 +0100

    Adding try catch

--- src/fetch.js
+++ src/fetch.js
@@ -1,17 +1,15 @@
-const fetchStarWarsData = async (type) => {
-  const response = await fetch("https://swapi.dev/api/" + type).catch((err) => {
-    console.log("rejected:", err.message);
-    return { error: err.message };
-  });
-  if (response.status !== 200) {
-    throw new Error("can't fetch the data");
+export default fetchStarWarsData = async (type) => {
+  try {
+    const response = await fetch("https://swapi.dev/api/" + type);
+
+    if (response.status !== 200) {
+      throw new Error("can't fetch the data");
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (e) {
+    console.log("Error", e);
   }
-
-  const data = await response.json();
-
-  return data;
 };
-
-// fetchStarWarsData("people"); // przykład
-
-export default fetchStarWarsData;
