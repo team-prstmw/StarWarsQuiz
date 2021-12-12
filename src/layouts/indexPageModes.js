2c43b9cdb1a3eff3260c14aa7cc2291891d37819
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2021-12-13 00:34:23 +0100

    minor corrections, punctuation in IndexPageModes

--- src/layouts/indexPageModes.js
+++ src/layouts/indexPageModes.js
@@ -1,52 +1,50 @@
-import Header from '../components/header/Header'
-import Container from '../components/container/Container'
-import Description from '../components/description/Description'
+import Header from '../components/header/Header';
+import Container from '../components/container/Container';
+import Description from '../components/description/Description';
 
-export class indexPageModes {
-    constructor() {
-        this.container = null        
-    }    
+export class IndexPageModes {
+  render() {
+    const container = document.createElement('section');
 
-    render() {
-        if (!this.container) {
-            this.container = document.createElement('section')
-        }
-        this.container.innerHTML = ''
-        this.container.classList.add('index-page__container')
-        
-        this.container.style.gridArea='mode-description'
-        this.container.style.margin='0 3rem'
-        this.container.style.alignSelf='start'    
-                
-        const containerElement1 = new Container(['main__header-container'])
-        const headerElement1= new Header('MODE: Who is this character?',['main__header'], "h2")
+    container.innerHTML = '';
+    container.classList.add('index-page__container');
 
-        const modesContainerElement = new Container(['main__modes-container'])
-        const headerElement2= new Header('Mode Rules',['main__subheader'], "h3")
-        const descriptionElement= new Description('You have one minute (1m) to answer as many questions as possible. During the game on each question you need to select who from Star Wars is showed on the left (Jar Jar Binks right now) from available options.',['main__description'])    
-        
-        const HeaderContainerElement = new Container(['main__subheader-container'])
-        const HeaderContainerElementRendered = HeaderContainerElement.render()
-          
-        const containerElement1Rendered = containerElement1.render()
-        containerElement1Rendered.appendChild(headerElement1.render())
+    container.style.gridArea = 'mode-description';
+    container.style.margin = '0 3rem';
+    container.style.alignSelf = 'start';
 
-        const modesContainerElementRendered = modesContainerElement.render()
+    const containerElement1 = new Container(['main__header-container'], 'div');
+    const headerElement1 = new Header('MODE: Who is this character?', ['main__header'], 'h2');
 
-        const graduationCap_img = document.createElement("img");
-        graduationCap_img.src = "/school_24px.svg";
-        
-        HeaderContainerElementRendered.appendChild(graduationCap_img)
-        HeaderContainerElementRendered.appendChild(headerElement2.render())
-        
-        modesContainerElementRendered.appendChild(HeaderContainerElementRendered)
-        modesContainerElementRendered.appendChild(descriptionElement.render())
-       
-        this.container.appendChild(containerElement1Rendered)
-        this.container.appendChild(modesContainerElementRendered)
+    const modesContainerElement = new Container(['main__modes-container'], 'div');
+    const headerElement2 = new Header('Mode Rules', ['main__subheader'], 'h3');
+    const descriptionElement = new Description(
+      'You have one minute (1m) to answer as many questions as possible. During the game on each question you need to select who from Star Wars is showed on the left (Jar Jar Binks right now) from available options.',
+      ['main__description']
+    );
 
-        return this.container
-    }
+    const HeaderContainerElement = new Container(['main__subheader-container']);
+    const HeaderContainerElementRendered = HeaderContainerElement.render();
+
+    const containerElement1Rendered = containerElement1.render();
+    containerElement1Rendered.appendChild(headerElement1.show());
+
+    const modesContainerElementRendered = modesContainerElement.render();
+
+    const graduationCapImage = document.createElement('img');
+    graduationCapImage.src = '/school_24px.svg';
+
+    HeaderContainerElementRendered.appendChild(graduationCapImage);
+    HeaderContainerElementRendered.appendChild(headerElement2.show());
+
+    modesContainerElementRendered.appendChild(HeaderContainerElementRendered);
+    modesContainerElementRendered.appendChild(descriptionElement.show());
+
+    container.appendChild(containerElement1Rendered);
+    container.appendChild(modesContainerElementRendered);
+
+    return container;
+  }
 }
 
-export default indexPageModes
+export default IndexPageModes;
