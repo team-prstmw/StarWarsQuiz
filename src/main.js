5a0571dc48b5154b54e196a09874b0915df518d9
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2021-12-13 00:36:18 +0100

    refactoring of main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
-import indexPageModes from "./layouts/indexPageModes"
-import changeGameMode from "./changeGameMode"
+import IndexPageModes from './layouts/IndexPageModes';
+import { addListenerToButtons } from './changeGameMode';
 
-const indexPage = new indexPageModes()
+const indexPage = new IndexPageModes();
 
-document.getElementById("main-grid-container").appendChild(indexPage.render())
+document.getElementById('main-grid-container').appendChild(indexPage.render());
 
-const btnChar = document.querySelector('.btn-characters')
-btnChar.addEventListener('click', ()=> changeGameMode('.main__header', '.main__description',"MODE: Who is this character?", 'You have one minute (1m) to answer as many questions as possible. During the game on each question you need to select who from Star Wars is showed on the left (Jar Jar Binks right now) from available options.'))
-
-const btnVehicles = document.querySelector('.btn-vehicles')
-btnVehicles.addEventListener('click', ()=> changeGameMode('.main__header','.main__description',"MODE: What kind of vehicle is this?", "You have one minute (1m) to answer as many questions as possible. During the game you need to select what vehicle from Star Wars is showed in the picture."))
-
-const btnStarships = document.querySelector('.btn-starships')
-btnStarships.addEventListener('click', ()=> changeGameMode('.main__header','.main__description',"MODE: What kind of starship is this?", "You have one minute (1m) to answer as many questions as possible. During the game you need to select what starship from Star Wars is showed in the picture."))
-
+document.addListenerToButtons = addListenerToButtons(
+  ['.btn-characters', '.btn-vehicles', '.btn-starships'],
+  '.main__header',
+  '.main__description',
+  ['Who is this character?', 'What kind of vehicle is this?', 'What kind of starship is this?'],
+  [
+    'who from Star Wars is showed on the left (Jar Jar Binks right now) from available options.',
+    'what vehicle from Star Wars is showed in the picture.',
+    'what starship from Star Wars is showed in the picture.',
+  ]
+);
