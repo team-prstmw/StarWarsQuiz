7b24fd31374da7fa824c6079b3e23c80c094c5c4
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2021-12-13 00:24:15 +0100

    extending Header class by HtmlElement class

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,25 +1,17 @@
+import HtmlElement from '../htmlElement/HtmlElement';
 import './styles.css';
 
-export class Header {
-    constructor(label='', classList=[], headerTag=null) {
-        this.label = label        
-        this.classList = classList
-        this.headerTag = headerTag
-    }
+export class Header extends HtmlElement {
+  constructor(label = '', classList = [], htmlTag = null) {
+    super(classList, htmlTag);
+    this.label = label;
+  }
 
-    render() {       
-        const header = document.createElement(this.headerTag)
-
-        header.innerText = this.label
-
-        if (Array.isArray(this.classList)) {
-            this.classList.forEach(element => {
-                header.classList.add(element)
-            })
-        } 
-          
-        return header
-    }
+  show() {
+    const headerElement = this.render();
+    headerElement.innerText = this.label;
+    return headerElement;
+  }
 }
 
-export default Header
+export default Header;
