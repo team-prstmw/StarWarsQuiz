55474262135896e65d4f5f55f66a763c2cca1a29
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2021-12-13 00:30:57 +0100

    added addListenerToButtons func in changeGameMode

--- src/changeGameMode.js
+++ src/changeGameMode.js
@@ -1,6 +1,23 @@
-export const changeGameMode = (htmlElementQuestion, htmlElementQuestionDescription, text, description) =>{    
-    document.querySelector(htmlElementQuestion).innerHTML = text;
-    document.querySelector(htmlElementQuestionDescription).innerHTML = description;
-}
+export const changeGameMode = (htmlElementQuestion, htmlElementQuestionDescription, text, description) => {
+  document.querySelector(htmlElementQuestion).innerHTML = `MODE: ${text}`;
+  document.querySelector(
+    htmlElementQuestionDescription
+  ).innerHTML = `You have one minute (1m) to answer as many questions as possible. During the game on each question you need to select ${description}`;
+};
 
-export default changeGameMode
+export const addListenerToButtons = (
+  btnArr = [],
+  htmlElementQuestion,
+  htmlElementQuestionDescription,
+  text,
+  description
+) => {
+  btnArr.forEach((btnClass, index) => {
+    const btn = document.querySelector(btnClass);
+    btn.addEventListener('click', () =>
+      changeGameMode(htmlElementQuestion, htmlElementQuestionDescription, text[index], description[index])
+    );
+  });
+};
+
+export default changeGameMode;
