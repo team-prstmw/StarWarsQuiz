4ef48491b3e5a3590f375af341bd9f3c08cc0e5d
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2021-12-13 00:23:27 +0100

    extending Container class by HtmlElement class

--- src/components/container/Container.js
+++ src/components/container/Container.js
@@ -1,20 +1,9 @@
+import HtmlElement from '../htmlElement/HtmlElement';
 import './styles.css';
 
-export class Container {
-  constructor(classList = []) {
-    this.classList = classList;
-  }
-
-  render() {
-    const container = document.createElement('div');
-
-    if (Array.isArray(this.classList)) {
-      this.classList.forEach((element) => {
-        container.classList.add(element);
-      });
-    }
-
-    return container;
+export class Container extends HtmlElement {
+  constructor(classList = [], htmlTag) {
+    super(classList, htmlTag);
   }
 }
 
