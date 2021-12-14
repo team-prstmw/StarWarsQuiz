f3c996de60b41243e96a4047610580d2d922c46e
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2021-12-14 17:44:13 +0100

    refactoring main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import IndexPageModes from './layouts/IndexPageModes';
+import IndexPageModes from './layouts/IndexPageModes/IndexPageModes';
 import { addListenerToButtons } from './changeGameMode';
 
 const indexPage = new IndexPageModes();
@@ -7,8 +7,6 @@ document.getElementById('main-grid-container').appendChild(indexPage.render());
 
 document.addListenerToButtons = addListenerToButtons(
   ['.btn-characters', '.btn-vehicles', '.btn-starships'],
-  '.main__header',
-  '.main__description',
   ['Who is this character?', 'What kind of vehicle is this?', 'What kind of starship is this?'],
   [
     'who from Star Wars is showed on the left (Jar Jar Binks right now) from available options.',
