df0578655a58d888cac58de5e71e43dca07627e6
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2021-12-14 17:42:33 +0100

    refactoring of changeGameMode function

--- src/changeGameMode.js
+++ src/changeGameMode.js
@@ -1,22 +1,14 @@
-export const changeGameMode = (htmlElementQuestion, htmlElementQuestionDescription, text, description) => {
-  document.querySelector(htmlElementQuestion).innerHTML = `MODE: ${text}`;
+export const changeGameMode = (text, description) => {
+  document.querySelector('.main__header').innerHTML = `MODE: ${text}`;
   document.querySelector(
-    htmlElementQuestionDescription
+    '.main__description'
   ).innerHTML = `You have one minute (1m) to answer as many questions as possible. During the game on each question you need to select ${description}`;
 };
 
-export const addListenerToButtons = (
-  btnArr = [],
-  htmlElementQuestion,
-  htmlElementQuestionDescription,
-  text,
-  description
-) => {
+export const addListenerToButtons = (btnArr = [], text, description) => {
   btnArr.forEach((btnClass, index) => {
     const btn = document.querySelector(btnClass);
-    btn.addEventListener('click', () =>
-      changeGameMode(htmlElementQuestion, htmlElementQuestionDescription, text[index], description[index])
-    );
+    btn.addEventListener('click', () => changeGameMode(text[index], description[index]));
   });
 };
 
