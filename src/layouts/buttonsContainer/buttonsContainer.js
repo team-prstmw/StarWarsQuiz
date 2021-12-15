67f7f55443f35a40b5ec8656ae5587cc69b362fe
Marta Goniszewska <marta.goniszewska@outlook.com>
2021-12-15 21:15:48 +0100

    Improving responsiveness

--- src/layouts/buttonsContainer/buttonsContainer.js
+++ src/layouts/buttonsContainer/buttonsContainer.js
@@ -1,4 +1,5 @@
-import Button from '../components/button/Button';
+import Button from '../../components/button/Button';
+import './styles.css';
 
 export class ButtonsContainer {
   constructor() {
@@ -8,16 +9,8 @@ export class ButtonsContainer {
   render() {
     if (!this.container) {
       this.container = document.createElement('section');
+      this.container.className = 'container';
     }
-    this.container.innerHTML = '';
-    this.container.classList.add('buttons__container');
-    this.container.style.gridArea = 'buttons';
-    this.container.style.margin = '1rem 0';
-    this.container.style.alignSelf = 'start';
-    this.container.style.minWidth = '100%';
-    this.container.style.gap = '10px';
-    this.container.style.display = 'flex';
-    this.container.style.justifyContent = 'space-between';
 
     // const ButtonSettings = new Button({ label: 'Settings', type: 'default', width: 'full' });
     const buttonHof = new Button({
