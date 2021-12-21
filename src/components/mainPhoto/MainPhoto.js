9200fd8e1ea7380c34722b5da219a48f49e68d89
shadowas-py <glas1690@gmail.com>
2021-12-21 18:43:06 +0100

    Html element now have props parameter

--- src/components/mainPhoto/MainPhoto.js
+++ src/components/mainPhoto/MainPhoto.js
@@ -1,17 +1,12 @@
 import HtmlElement from '../htmlElement/HtmlElement';
 
 export default class MainPhoto extends HtmlElement {
-  constructor(id = 'main-photo', imageSrc = 'images/QuestionPhoto1.png', width = '200px', height = '250px') {
-    super(['main-photo'], 'img', "It's main image");
+  constructor(id = 'main-photo') {
+    super(['main-photo'], 'img', "It's main image", {
+      src: 'images/QuestionPhoto1.png',
+      width: '200px',
+      height: '250px',
+    });
     this.id = id;
-    this.imageSrc = imageSrc;
-    this.width = width;
-    this.height = height;
-  }
-
-  render() {
-    const img = super.render();
-
-    return img;
   }
 }
