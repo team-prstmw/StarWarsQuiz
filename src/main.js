446228764807b9a21b33c39ba6074fcb80b1ce2b
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2021-12-21 21:31:45 +0100

    temporary setup of error page to avoid issues with vite

--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
-import IndexPageModes from './layouts/IndexPageModes/IndexPageModes';
-import { addListenerToButtons } from './utils/changeGameMode';
-import ButtonsContainer from './layouts/buttonsContainer/buttonsContainer';
+// import IndexPageModes from './layouts/IndexPageModes/IndexPageModes';
+// import { addListenerToButtons } from './utils/changeGameMode';
+// import ButtonsContainer from './layouts/buttonsContainer/buttonsContainer';
+import ErrorPage from './layouts/errorPage/ErrorPage';
 
-const indexPage = new IndexPageModes();
+// const indexPage = new IndexPageModes();
+const errorPage = new ErrorPage();
 
-const buttonsContainer = new ButtonsContainer();
+// const buttonsContainer = new ButtonsContainer();
 
-document.getElementById('main-grid-container').appendChild(indexPage.render()).appendChild(buttonsContainer.render());
+// document.getElementById('main-grid-container').appendChild(indexPage.render()).appendChild(buttonsContainer.render());
+document.body.appendChild(errorPage.render());
 
-document.addListenerToButtons = addListenerToButtons(
-  ['.btn-characters', '.btn-vehicles', '.btn-starships'],
-  ['Who is this character?', 'What kind of vehicle is this?', 'What kind of starship is this?'],
-  [
-    'who from Star Wars is showed on the left (Jar Jar Binks right now) from available options.',
-    'what vehicle from Star Wars is showed in the picture.',
-    'what starship from Star Wars is showed in the picture.',
-  ]
-);
+// document.addListenerToButtons = addListenerToButtons(
+//   ['.btn-characters', '.btn-vehicles', '.btn-starships'],
+//   ['Who is this character?', 'What kind of vehicle is this?', 'What kind of starship is this?'],
+//   [
+//     'who from Star Wars is showed on the left (Jar Jar Binks right now) from available options.',
+//     'what vehicle from Star Wars is showed in the picture.',
+//     'what starship from Star Wars is showed in the picture.',
+//   ]
+// );
