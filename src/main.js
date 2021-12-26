27929e1fa2070679f7754967bd9adcab6ba4e80b
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2021-12-26 11:32:17 +0100

    listeners added in the main.js to close intro on keyboard press

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import IndexPageModes from './layouts/IndexPageModes/IndexPageModes';
 import { addListenerToButtons } from './utils/changeGameMode';
 import ButtonsContainer from './layouts/buttonsContainer/buttonsContainer';
+import StarWarsIntro from './layouts/StarWarsIntro/StarWarsIntro';
+import { findLastElementPosition, hideStarWarsIntro } from './utils/hideStarWarsIntro';
+import tapHandler from './utils/doubleTapHandler';
 
 const indexPage = new IndexPageModes();
+const starWarsIntro = new StarWarsIntro();
 
 const buttonsContainer = new ButtonsContainer();
+
+document.body.appendChild(starWarsIntro.render());
 
 document.getElementById('main-grid-container').appendChild(indexPage.render()).appendChild(buttonsContainer.render());
 
@@ -16,4 +22,26 @@ document.addListenerToButtons = addListenerToButtons(
     'what vehicle from Star Wars is showed in the picture.',
     'what starship from Star Wars is showed in the picture.',
   ]
+);
+
+const intervalId = setInterval(() => {
+  findLastElementPosition();
+}, 1000);
+
+document.addEventListener('keydown', (event) => {
+  if (document.querySelector('.intro__container') && event.key === 'Escape') {
+    hideStarWarsIntro();
+    clearInterval(intervalId);
+  }
+});
+
+document.addEventListener(
+  'touchstart',
+  (e) => {
+    if (document.querySelector('.intro__container')) {
+      tapHandler(e);
+      clearInterval(intervalId);
+    }
+  },
+  { passive: false }
 );
