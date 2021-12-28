15a64cfe37e1d00440d6c85e73f7240b8dcfe41f
shadowas-py <glas1690@gmail.com>
2021-12-28 01:57:32 +0100

    trying to make it work

--- src/components/photoOnTheLeft/PhotoOnTheLeft.js
+++ src/components/photoOnTheLeft/PhotoOnTheLeft.js
@@ -1,13 +1,13 @@
 import HtmlElement from '../htmlElement/HtmlElement';
 import './photoOnTheLeft.css';
-import getRandomImgSrc from './getRandomImage';
+// import getRandomImgSrc from './getRandomImage';
 
 class MainPhoto extends HtmlElement {
   constructor() {
     super(['photo-on-the-left'], 'img', 'Picture should be here', {
       id: 'photo-on-the-left',
-      src: getRandomImgSrc().then((data) => data.src),
-      category: getRandomImgSrc().then((data) => data.category),
+      src: '',
+      category: '',
     });
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,17 +3,26 @@ import { addListenerToButtons } from './utils/changeGameMode';
 import ButtonsContainer from './layouts/buttonsContainer/buttonsContainer';
 import MainPhoto from './components/photoOnTheLeft/PhotoOnTheLeft';
 import textRelatedtoPhotoCategory from './components/photoOnTheLeft/textRelatedToPhotoCategory';
+import getRandomImgSrc from './components/photoOnTheLeft/getRandomImage';
 
 const indexPage = new IndexPageModes();
 
 const buttonsContainer = new ButtonsContainer();
 
 const mainPhoto = new MainPhoto();
+
 document.getElementById('main-grid-container').appendChild(mainPhoto.render());
 
 document.getElementById('main-grid-container').appendChild(indexPage.render()).appendChild(buttonsContainer.render());
 
-document.querySelector('h2.main__header').innerHTML = textRelatedtoPhotoCategory.mainPhoto.category;
+getRandomImgSrc().then((res) => {
+  document.getElementById('photo-on-the-left').src = res.src;
+  document.getElementById('photo-on-the-left').category = res.category;
+});
+
+const { category } = document.querySelector('.photo-on-the-left').category;
+
+document.querySelector('h2.main__header').innerHTML = textRelatedtoPhotoCategory[category];
 
 document.addListenerToButtons = addListenerToButtons(
   ['.btn-characters', '.btn-vehicles', '.btn-starships'],
