8191e06896f7c60a984f59f47665b4758a1f269f
mgoniszewska <marta.goniszewska@outlook.com>
2022-01-02 12:37:22 +0100

    Changes after cr and fancy scrollbar

--- src/components/answersList/answersList.js
+++ src/components/answersList/answersList.js
@@ -3,36 +3,36 @@ import './styles.css';
 import Answer from '../answer/answer';
 
 class AnswersList extends HtmlElement {
-  constructor( answers = []) {
+  constructor(answers = []) {
     super(['answers-list-container'], 'div');
     this.answers = answers;
   }
 
   render() {
-const answersListTable = document.createElement('table');
-answersListTable.className = 'answers-list-table';
+    const answersListTable = document.createElement('table');
+    answersListTable.className = 'answers-list-table';
 
-const answersListTableHead = document.createElement('thead');
-answersListTableHead.className = 'answers-list-thead';
+    const answersListTableHead = document.createElement('thead');
+    answersListTableHead.className = 'answers-list-thead';
 
-const answersListTableHeaderRow = document.createElement('tr');
-answersListTableHeaderRow.className = 'answers-header-row';
+    const answersListTableHeaderRow = document.createElement('tr');
+    answersListTableHeaderRow.className = 'answers-header-row';
 
-const headers = ['', 'You', 'Answer'];
+    const headers = ['', 'You', 'Answer'];
 
-headers.forEach( header => {
-  const answersHeader = document.createElement('th');
-  answersHeader.className = 'answers-header'
-  answersHeader.innerText = header
-  answersListTableHeaderRow.append(answersHeader);
-})
+    headers.forEach((header) => {
+      const answersHeader = document.createElement('th');
+      answersHeader.className = 'answers-header';
+      answersHeader.innerText = header;
+      answersListTableHeaderRow.append(answersHeader);
+    });
 
-answersListTableHead.append(answersListTableHeaderRow)
-answersListTable.append(answersListTableHead)
+    answersListTableHead.append(answersListTableHeaderRow);
+    answersListTable.append(answersListTableHead);
 
-const answersListTableBody = document.createElement('tbody')
-answersListTableBody.className = 'answers-list-tbody'
-answersListTable.append(answersListTableBody)
+    const answersListTableBody = document.createElement('tbody');
+    answersListTableBody.className = 'answers-list-tbody';
+    answersListTable.append(answersListTableBody);
 
     this.answers.forEach(({ image, userAnswer, correctAnswer }) => {
       const answer = new Answer({ image, userAnswer, correctAnswer });
