53c315eed1e2fb510f3cb0020a322c1e8143492b
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2022-01-02 21:18:35 +0100

    SOUNDS_IDs paths to DB added

---
+++ src/components/mainPhoto/allSounds.js
@@ -0,0 +1,18 @@
+const SOUNDS_IDs = [
+  'people/1',
+  'people/2',
+  'people/3',
+  'people/4',
+  'people/5',
+  'people/10',
+  'people/13',
+  'people/14',
+  'people/15',
+  'people/16',
+  'people/20',
+  'people/21',
+  'people/22',
+  'people/27',
+];
+
+export default SOUNDS_IDs;
