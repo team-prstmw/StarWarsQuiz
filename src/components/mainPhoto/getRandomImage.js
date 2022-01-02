8592c694174beeb0e60d8a5e69d7db8710e9f4d9
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2022-01-02 21:19:16 +0100

    playSound function implemented inside getRandomImage function

--- src/components/mainPhoto/getRandomImage.js
+++ src/components/mainPhoto/getRandomImage.js
@@ -1,4 +1,6 @@
+import playSound from '../../playSound';
 import IMAGES_IDs from './all_Images';
+import SOUNDS_IDs from './allSounds';
 
 function getRandomImage(category = '') {
   if (!category) {
@@ -9,7 +11,16 @@ function getRandomImage(category = '') {
 
   const photosInCategory = IMAGES_IDs[category];
   const randomImageID = photosInCategory[Math.floor(Math.random() * photosInCategory.length)];
-  const source = `../../../images/modes/${randomImageID}.jpg`;
+  let source = `../../../images/modes/${randomImageID}.jpg`;
+
+  // TO TEST PLAY CHARACTERS VOICES PLEASE UNCOMMENT CODE BELOW AND COMMENT ALL 2 LINES ABOVE
+  // const randomImageID = 'people/1';
+  // let source = `../../../images/modes/people/1.jpg`;
+
+  if (SOUNDS_IDs.includes(randomImageID)) {
+    playSound(`/assets/sounds/charactersVoices/${randomImageID}.mp3`);
+  }
+
   return source;
 }
 export default getRandomImage;
