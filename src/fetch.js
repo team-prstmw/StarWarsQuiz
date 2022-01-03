a8d97e05e52cd04c7cece462af82d7f9895a3951
Wojciech Okularczyk <wojciech.okularczyk@coderscrew.pl>
2022-01-03 23:29:36 +0100

    Fetching and displaying random answers

--- src/fetch.js
+++ src/fetch.js
@@ -1,7 +1,6 @@
 const fetchStarWarsData = async (type) => {
   try {
     const response = await fetch(`https://swapi.dev/api/${type}`);
-
     if (response.status !== 200) {
       throw new Error("can't fetch the data");
     }
--- src/quizPage.js
+++ src/quizPage.js
@@ -2,6 +2,7 @@ import QuizContainer from './quizPageElements/QuizContainer';
 import MainPhoto from './components/mainPhoto/MainPhoto';
 import fetchStarWarsData from './fetch';
 import all_Images from './components/mainPhoto/all_Images';
+import shuffle from './utils/shuffle';
 
 document.MODES = {
   people: 'Who is this character?',
@@ -20,17 +21,32 @@ document.getElementById('main-grid-container').appendChild(mainPhoto.render());
 const splitted = document.getElementById('main-photo').src.split('/');
 document.mode = splitted[splitted.length - 2];
 
-const correctAnswerId = document.mode + '/' + splitted[splitted.length - 1].split('.')[0];
+const answerInternalID = splitted[splitted.length - 1].split('.')[0];
+const correctAnswerId = document.mode + '/' + answerInternalID;
 
 fetchStarWarsData(correctAnswerId).then((data) => {
   document.answer = data.name;
-  document.setOfQuestion.splice(correctAnswerId, 1);
-  document.fetchedAnswersList = [document.answer, 'answer B', 'answer C', 'answer D'];
-  const quiz = new QuizContainer(modeDesc, document.fetchedAnswersList);
-  document.getElementById('main-grid-container').appendChild(quiz.render());
+  fetchStarWarsData(document.mode).then(async (data) => {
+    const indexesToFetch = [];
+    while (indexesToFetch.length < 3) {
+      const randomArrayIndex = Math.floor(Math.random() * data.count);
+      if (indexesToFetch.indexOf(randomArrayIndex) < 0) {
+        indexesToFetch.push(randomArrayIndex);
+      }
+    }
+    const fakes = await Promise.all(
+      indexesToFetch.map(async (index) => {
+        const pageNr = Math.floor(index / 10) + 1;
+        const indexNr = index % 10;
+        const source = `${document.mode}/?page=${pageNr}`;
+        return await fetchStarWarsData(source).then((data) => data.results[indexNr].name);
+      })
+    );
+    document.fetchedAnswersList = [document.answer, ...fakes];
+    const quiz = new QuizContainer(modeDesc, document.fetchedAnswersList);
+    document.getElementById('main-grid-container').appendChild(quiz.render());
+  });
 });
-
-// fetchWrongAnswers();
 // handleSetOfQuestions();
 // randomize answers order
 // collecting answers
