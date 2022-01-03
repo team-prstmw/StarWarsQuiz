e7e1245ea62c20ed56a36e2ad9ac7f67b0aabb01
shadowas-py <glas1690@gmail.com>
2022-01-03 14:34:45 +0100

    cleaning code / changed active button style

--- src/components/mainPhoto/getRandomImage.js
+++ src/components/mainPhoto/getRandomImage.js
@@ -1,11 +1,11 @@
 import IMAGES_IDs from './all_Images';
 
 function getRandomImage(category = '') {
-  if (!category) {
-    // return random from all pictures in IMAGES_IDs
-    source = '../../../images/Yoda.png';
-    return '../../../images/Yoda.png';
-  }
+  // if (!category) {
+  //   // return random from all pictures in IMAGES_IDs
+  //   source = '../../../images/Yoda.png';
+  //   return '../../../images/Yoda.png';
+  // }
 
   const photosInCategory = IMAGES_IDs[category];
   const randomImageID = photosInCategory[Math.floor(Math.random() * photosInCategory.length)];
