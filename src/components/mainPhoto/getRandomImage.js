f3367a8fcf4c75ba35e5efcb4015df584d1ab044
Wojciech Okularczyk <wojciech.okularczyk@coderscrew.pl>
2022-01-06 14:19:09 +0100

    applied Sebastian's changes

--- src/components/mainPhoto/getRandomImage.js
+++ src/components/mainPhoto/getRandomImage.js
@@ -1,7 +1,6 @@
 import IMAGES_IDs from './all_Images';
 
 function getRandomImage(category = '', imagesCollection = []) {
-  console.log(category, imagesCollection);
   const photosInCategory = !imagesCollection.length ? IMAGES_IDs[category] : imagesCollection;
   const randomImageID = photosInCategory[Math.floor(Math.random() * photosInCategory.length)];
   const source = `../../../images/modes/${randomImageID}.jpg`;
